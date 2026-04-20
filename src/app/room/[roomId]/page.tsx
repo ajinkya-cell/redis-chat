62fc342137fea4b7e66d67f6e987195e46cf48a3
ajinkya-cell <ajinkyaadharkar@gmail.com>
2026-04-20 14:31:09 +0530

    completed project

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,6 +1,12 @@
 "use client"
-import { useParams } from "next/navigation"
-import { useRef, useState } from "react"
+import { client } from "@/lib/client"
+import { useUsername } from "@/app/hooks/use-username"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { useParams, useRouter } from "next/navigation"
+import { useEffect, useRef, useState } from "react"
+import { format } from "date-fns"
+import { useRealtime } from "@/lib/realtime-client"
+
 
 function formatTimeRemaining (seconds : number){
     const mins = Math.floor(seconds/60)
@@ -12,11 +18,83 @@ const Page = () =>{
 
     const params = useParams()
     const roomId = params.roomId as string
+    const router = useRouter()
+    const { username } = useUsername()
 
     const[input , setInput] = useState("")
     const inputRef = useRef<HTMLInputElement>(null)
     const [copyStatus , setCopyStatus] = useState("Copy")
     const [timeRemaining , setTimeRemaining] = useState<number | null>(null)
+
+    const {data : ttlData} = useQuery({
+        queryKey:["ttl" , roomId],
+        queryFn : async()=>{
+            const res = await client.room.ttl.get({
+                query:{roomId}
+            })
+            return res.data
+        }
+    })
+    useEffect(()=>{
+        if(ttlData?.ttl!==undefined)
+            setTimeRemaining(ttlData.ttl)
+    },[ttlData])
+
+    useEffect(()=>{
+        if(timeRemaining ===null || timeRemaining<0) return
+        if(timeRemaining ===0){
+            router.push("/?destroyed=true")
+            return
+        }
+
+        const interval = setInterval(()=>{
+            setTimeRemaining((prev)=>{
+                if(prev ===null || prev <=1){
+                    clearInterval(interval)
+                    return 0
+                }
+                return prev -1
+            })
+        },1000)
+        return ()=>clearInterval(interval)
+    },[timeRemaining, router])
+
+   const {data : messages , refetch} = useQuery({
+    queryKey :["messages" , roomId],
+    queryFn:async()=>{
+        const res = await client.messages.get({
+            query:{roomId}
+            
+        })
+        return res.data
+    }
+   })
+
+    const { mutate: sendMessage ,isPending  } = useMutation({
+        mutationFn:async({text}:{text : string}) =>{
+            await client.messages.post({sender:username, text},{query:{roomId}})
+            setInput("")
+        }
+    })
+
+    useRealtime({
+        channels:[roomId],
+        events :['chat.message' , 'chat.destroy'],
+        onData :({event})=>{
+            if(event ==='chat.message'){
+                refetch()
+            }
+            if(event ==="chat.destroy"){
+                router.push("/?destroyed=true")
+            }
+        }
+    })
+
+    const{mutate : destroyRoom}= useMutation({
+        mutationFn:async()=>{
+            await client.room.delete(null , {query:{roomId}})
+        }
+    })
 
     const copyLink = () =>{
         const url = window.location.href
@@ -53,7 +131,7 @@ const Page = () =>{
             </div>
             </div>
 
-            <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center gap-2 disabled:opacity-50">
+            <button onClick={()=>destroyRoom()} className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center gap-2 disabled:opacity-50">
                 <span className="group-hover:animate-pulse">
                     💣
                 </span>
@@ -61,7 +139,29 @@ const Page = () =>{
             </button>
             </header>
             <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin" >
+                {messages?.messages.length===0 && (
+                    <div className="flex items-center justify-center h-full">
+                        <p className="text-zinc-600 font-mono">
+                            No messages yet , start the conversation
+                        </p>
+                    </div>
+                )}
 
+                {messages?.messages.map((msg)=>(
+                    <div key={msg.id} className="flex flex-col items-start">
+                        <div className="max-w-[80%] group">
+                            <div className="flex items-baseline gap-3 mb-1">
+                                <span className={`text font-bold ${msg.sender === username ? 'text-green-500': "text-blue-500"}`}>
+                                    {msg.sender ===username?"YOU ":msg.sender}
+                                </span>
+                                <span className="text-[10px] text-zinc-600">
+                                    {format(msg.timeStamp,"HH:mm")}
+                                </span>
+                            </div>
+                            <p className="text-sm text-zinc-200 leading-relaxed break-all">{msg.text}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
             <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
             <div className="flex gap-4">
@@ -72,13 +172,19 @@ const Page = () =>{
                     <input type="text"
                      value={input}
                      onKeyDown={(e)=> {if(e.key ==="Enter" && input.trim()){
+                        sendMessage({text:input})
                         inputRef.current?.focus()
                      }}}
                      placeholder="Type message ..."
                      onChange={(e) => setInput(e.target.value)} className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm" />
                 </div>
 
-                <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer" > 
+                <button onClick={()=>{
+                    sendMessage({text:input})
+                    inputRef.current?.focus()
+                }} 
+                disabled={!input.trim()|| isPending}
+                className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer" > 
                     SEND
                 </button>
             </div>
