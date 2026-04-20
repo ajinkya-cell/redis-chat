aebfa1b9c375391d05499259c85f5897e41d7564
ajinkya-cell <ajinkyaadharkar@gmail.com>
2026-04-20 23:44:30 +0530

    some changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,11 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+const getApiUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin
+  }
+  return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+}
+
+export const client = treaty<App>(getApiUrl()).api
